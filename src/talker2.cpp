e50cd8e46aa042235ee5ccafbf57103f6fa1a1ce
Fangjun Kuang <csukuangfj@gmail.com>
2017-12-10 11:35:40 +0100

    refactoring.

--- src/talker2.cpp
+++ src/talker2.cpp
@@ -16,10 +16,10 @@ class KfjTalker2 : public rclcpp::Node
 {
 public:
     KfjTalker2()
-        :Node(m_node_name),
+        :Node("talker2"),
           m_count(0)
     {
-        m_publisher = create_publisher<std_msgs::msg::String>(m_topic_name);
+        m_publisher = create_publisher<std_msgs::msg::String>("chatter");
         m_timer = create_wall_timer(500ms, std::bind(&KfjTalker2::timer_callback, this));
     }
 
@@ -29,7 +29,7 @@ private:
         auto message = std::make_shared<std_msgs::msg::String>();
         message->data = "Hello ROS2 " + std::to_string(m_count++);
         //RCLCPP_INFO(node->get_logger(), "Publishing: '%s'", message->data.c_str());
-        printf("Node %s: Publishing: '%s'\n", m_node_name.c_str(), message->data.c_str());
+        printf("Node %s: Publishing: '%s'\n", "talker2", message->data.c_str());
         m_publisher->publish(message);
         if (m_count > 20)
         {
@@ -41,9 +41,6 @@ private:
     size_t m_count;
     rclcpp::TimerBase::SharedPtr m_timer;
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr m_publisher;
-
-    const std::string m_node_name { "talker2" };
-    const std::string m_topic_name { "chatter" };
 };
 
 int main(int argc, char* argv[])
